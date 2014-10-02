2f5f83df914d538880d18c4fc709cd1abde3b139
Jason <bjason@gmail.com>
2014-10-01 23:40:57 -0700

    On ready state

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 console.log('hello');
 
 
-
-
 var generateDiv = function(text, index){
+
     var div = document.createElement("div");
     div.className = "item"
     div.innerText = text
 
+    return div;
 };
+
+var load = function(){
+
+    var div = generateDiv("yo", 0);
+    document.body.appendChild(div);
+};
+
+document.onreadystatechange = function(){
+    var state = document.readyState;
+    console.log('state', state);
+
+    if(state === 'interactive'){
+        /// WTF?
+        console.log('interactive');
+    } else if ( state === 'complete') {
+        console.log('complete');
+        load(); // TODO: make a comprise
+    }
+
+}
 
 
 window.onscroll = function(_event){
